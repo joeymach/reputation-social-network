ff725b059122d18dd3a35436f82bf824d946e39d
Your Name <ayoube.eddakhly@gmail.com>
2022-05-21 22:40:51 -0700

    fix

--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -2,15 +2,22 @@ import { Request, Response, Router } from 'express';
 import { getConnection } from "typeorm";
 import { mintNft } from './service';
 import { createClient } from 'redis';
+import * as nearAPI from "near-api-js";
+const fs = require("fs");
+import axios, { AxiosResponse } from 'axios';
+import { FilterBy } from '@redis/client/dist/lib/commands/COMMAND_LIST';
 
 /*
  Start redis:
     docker run --name some-redis -d redis redis-server --save 60 1 --loglevel warning
 */
 
+const { connect } = nearAPI;
+
 var port = 8080;
 var host = "127.0.0.1";
 var password = null;
+const rpcendpoint = "https://rpc.testnet.near.org";
 
 var redis = require("redis"),
     client = redis.createClient(port, host);
@@ -41,7 +48,17 @@ router.get('/account', async (req: Request, res: Response) => {
 
 router.get('/tokens', async (req: Request, res: Response) => {
     // TODO: Get any tokens belonging to the profile's NEAR account.
-    return res.status(200).json({});
+    // const { KeyPair, keyStores } = require("near-api-js");
+  let reso = await axios.get('https://testnet-api.kitwallet.app/account/blu3hackteam-test.testnet/activity')
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  let ress = reso
+  console.log(reso)
+  const aa = reso.map(a => )
+
+  return res.status(200).json({});
 });
 
 
