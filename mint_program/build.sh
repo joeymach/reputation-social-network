bfc3abd517b32b4b16d34ec1e6bd467ee68d95db
Joseph Caulfield <jcaulfield135@gmail.com>
2022-05-21 21:27:12 -0400

    near!

--- mint_program/build.sh
+++ mint_program/build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+if [[ $0 == "clean" ]]; then cargo clean; fi
+
 cargo build --target wasm32-unknown-unknown --release
---
+++ mint_program/deploy.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+near deploy --wasmFile target/wasm32-unknown-unknown/release/mint_program.wasm --accountId "blue3hackteam.testnet"
